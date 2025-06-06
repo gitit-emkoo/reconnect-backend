97cf62949640a82ab721d5284e038b5cc5444635
gitit-emkoo <kwcc2020@naver.com>
2025-06-06 22:31:24 +0900

    fix:빌드에러잡기(주석때문?)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users.module';
+import { UsersModule } from './users.module'; // <--- 이 경로가 올바른 경로입니다.
 import { EmotionCardsModule } from './emotion-cards/emotion-cards.module';
 import { CommunityModule } from './community/community.module';
-import { CoupleModule } from './couple/couple.module';
-import { ChallengeModule } from './challenge/challenge.module';
-import { EmotionCardModule } from './emotion-card/emotion-card.module';
-import { ReportModule } from './report/report.module';
-import { ContentModule } from './content/content.module';
-import { EmotionJournalModule } from './emotion-journal/emotion-journal.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -20,14 +15,14 @@ import { EmotionJournalModule } from './emotion-journal/emotion-journal.module';
     UsersModule,
     EmotionCardsModule,
     CommunityModule,
-    CoupleModule,
-    ChallengeModule,
-    EmotionCardModule,
-    ReportModule,
-    ContentModule,
-    EmotionJournalModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
