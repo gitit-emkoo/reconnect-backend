f6aebf68211c9df2ad7f11a6476abbdd30fde208
gitit-emkoo <kwcc2020@naver.com>
2025-06-07 11:26:58 +0900

    feat:커뮤니티페이지에서 태그로도 검색되게 수정

--- src/community/community.service.ts
+++ src/community/community.service.ts
@@ -69,6 +69,11 @@ export class CommunityService {
             mode: 'insensitive',
           },
         },
+        {
+          tags: {
+            has: search,
+          },
+        },
       ];
     }
 
