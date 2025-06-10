774b9608e39cfdfa565254794257971f8b704117
gitit-emkoo <kwcc2020@naver.com>
2025-06-10 21:02:48 +0900

    fix:유저 API수정

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Patch, Body, Req, UseGuards, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
-@Controller('api/users')
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
