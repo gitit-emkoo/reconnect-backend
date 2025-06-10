c24490eb328b9065c4a445687fbf6362d6201c94
gitit-emkoo <kwcc2020@naver.com>
2025-06-10 22:01:39 +0900

    fix:컨트롤러에서 잘못된 필드로 유저아이디넘긴것 수정

--- src/partner-invites/partner-invites.controller.ts
+++ src/partner-invites/partner-invites.controller.ts
@@ -9,7 +9,7 @@ export class PartnerInvitesController {
 
   @Post()
   async createInviteCode(@Request() req) {
-    return this.partnerInvitesService.createInviteCode(req.user.id);
+    return this.partnerInvitesService.createInviteCode(req.user.userId);
   }
 
   @Post('respond')
@@ -17,7 +17,7 @@ export class PartnerInvitesController {
     @Body() body: { code: string },
     @Request() req
   ) {
-    return this.partnerInvitesService.respondToInvite(body.code, req.user.id);
+    return this.partnerInvitesService.respondToInvite(body.code, req.user.userId);
   }
 
   @Post(':inviteId/accept')
@@ -25,7 +25,7 @@ export class PartnerInvitesController {
     @Param('inviteId') inviteId: string,
     @Request() req
   ) {
-    return this.partnerInvitesService.acceptInvite(inviteId, req.user.id);
+    return this.partnerInvitesService.acceptInvite(inviteId, req.user.userId);
   }
 
   @Post(':inviteId/reject')
@@ -33,16 +33,16 @@ export class PartnerInvitesController {
     @Param('inviteId') inviteId: string,
     @Request() req
   ) {
-    return this.partnerInvitesService.rejectInvite(inviteId, req.user.id);
+    return this.partnerInvitesService.rejectInvite(inviteId, req.user.userId);
   }
 
   @Get('me')
   async getMyInvites(@Request() req) {
     console.log('[PartnerInvitesController] req.user:', req.user);
     if (req.user) {
-      console.log('[PartnerInvitesController] req.user.id:', req.user.id);
+      console.log('[PartnerInvitesController] req.user.userId:', req.user.userId);
     }
-    const invites = await this.partnerInvitesService.getMyInvites(req.user.id);
+    const invites = await this.partnerInvitesService.getMyInvites(req.user.userId);
     console.log('[PartnerInvitesController] invites:', invites);
     return invites;
   }
