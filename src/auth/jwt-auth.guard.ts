d71ae3b069b25383d2be461269525b617b618d38
gitit-emkoo <kwcc2020@naver.com>
2025-06-10 20:12:43 +0900

    chore:JWT인증콘솔로그 추가

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {} 
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
+    if (err || !user) {
+      console.log('[JwtAuthGuard] 인증 실패:', { err, user, info });
+    }
+    return super.handleRequest(err, user, info, context);
+  }
+} 
