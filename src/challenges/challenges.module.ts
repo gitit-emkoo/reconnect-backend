ca17f4a3c9c0511da2546d57a9efedbf0efef4ea
gitit-emkoo <kwcc2020@naver.com>
2025-06-17 02:31:51 +0900

    fix:Add missing PrismaModule to ChallengesModule

--- src/challenges/challenges.module.ts
+++ src/challenges/challenges.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChallengesController } from './challenges.controller';
 import { ChallengesService } from './challenges.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ChallengesController],
   providers: [ChallengesService],
 })
