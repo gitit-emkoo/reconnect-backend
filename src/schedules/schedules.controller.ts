c020583e6e5d87cd92209e6b3553c9575a8b58d6
gitit-emkoo <kwcc2020@naver.com>
2025-06-18 14:02:22 +0900

    feat:스케줄 컨트롤러 호출 수정

--- src/schedules/schedules.controller.ts
+++ src/schedules/schedules.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { SchedulesService } from './schedules.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { GetUser } from '../auth/decorators/get-user.decorator';
+import { User } from '@prisma/client';
 
 @Controller('schedules')
+@UseGuards(JwtAuthGuard)
 export class SchedulesController {
   constructor(private readonly schedulesService: SchedulesService) {}
+
+  @Get()
+  findAll(@GetUser() user: User) {
+    return this.schedulesService.findAllByUserId(user.id);
+  }
 } 
