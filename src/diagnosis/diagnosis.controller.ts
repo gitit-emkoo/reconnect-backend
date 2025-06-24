4e758ca3e0388997f026aa13eb192b1c9e433ced
gitit-emkoo <kwcc2020@naver.com>
2025-06-24 11:50:19 +0900

    feat:리포트 페이지 감정온도 로직 계산식 준비

--- src/diagnosis/diagnosis.controller.ts
+++ src/diagnosis/diagnosis.controller.ts
@@ -1,20 +1,55 @@
-import { Controller, Get, Post, Body, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { DiagnosisService } from './diagnosis.service';
 import { CreateDiagnosisDto } from './dto/create-diagnosis.dto';
+import { UpdateDiagnosisDto } from './dto/update-diagnosis.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { GetUser } from '../auth/decorators/get-user.decorator';
+import type { User } from '@prisma/client';
 
 @Controller('diagnosis')
+@UseGuards(JwtAuthGuard)
 export class DiagnosisController {
   constructor(private readonly diagnosisService: DiagnosisService) {}
+
+  @Get('my-latest')
+  findMyLatest(@GetUser() user: User) {
+    return this.diagnosisService.findLatest(user.id);
+  }
 
   @Post()
   create(@Body() createDiagnosisDto: CreateDiagnosisDto) {
     return this.diagnosisService.create(createDiagnosisDto);
   }
 
-  @Get('latest')
-  @UseGuards(JwtAuthGuard)
-  findLatest(@Request() req) {
-    return this.diagnosisService.findLatest(req.user.userId);
+  @Get()
+  findAll() {
+    return this.diagnosisService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.diagnosisService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateDiagnosisDto: UpdateDiagnosisDto,
+  ) {
+    return this.diagnosisService.update(+id, updateDiagnosisDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.diagnosisService.remove(+id);
   }
 }
